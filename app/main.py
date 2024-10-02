8ec17af032f1f33fe9fabeb2b821f252a37b7095
Iyliya Cherepkova <iyliya.cherepkova@gmail.com>
2024-10-02 09:56:07 -0400

    Валидация для post и для выходных данных

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Depends
 from typing import Annotated
 from datetime import date
+from pydantic import BaseModel
 
 app = FastAPI(title="BookingApp",
     description="MDtext",
@@ -8,12 +9,39 @@ app = FastAPI(title="BookingApp",
     version="0.0.1",
 )
 
+class SHotel(BaseModel):
+    address: str
+    name: str
+    stars: int
+
+class HotelsSearchArgs:
+    def __init__(
+            self,
+            location: str,
+            date_from: date, 
+            date_to: date,
+            has_spa: bool = None,
+            stars: Annotated[int, Query(..., ge=1, le=5)] = None
+        ):
+        self.location = location
+        self.date_from = date_from
+        self.date_to = date_to
+        self.has_spa = has_spa
+        self.stars = stars
+
+
 @app.get('/hotels')
 def get_hotels(
-    location: str,
-    date_from: date, 
-    date_to: date,
-    has_spa: bool = None,
-    stars: Annotated[int, Query(..., ge=1, le=5)] = None
-    ):
-    return date_from, date_to
+    search_agrs: HotelsSearchArgs = Depends()
+):
+    return search_args
+
+class SBooking(BaseModel):
+    room_id: int
+    date_from: date
+    date_to: date
+
+
+@app.post('/bookings')
+def add_booking(booking: SBooking):
+    pass
