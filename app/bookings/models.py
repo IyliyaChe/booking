2c8d7a8d890a2e1f6ed58bcdaf31df54ea42d9fc
Iyliya Cherepkova <iyliya.cherepkova@gmail.com>
2024-10-11 10:32:41 -0400

    Модели алхимии в новом стиле, первый роутер

--- app/bookings/models.py
+++ app/bookings/models.py
@@ -1,7 +1,9 @@
 from sqlalchemy import JSON, Column, Computed, Date, ForeignKey, Integer, String
 from app.database import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from datetime import date
 
-class Bookings(Base):
+""" class Bookings(Base):
     __tablename__ = 'bookings'
     id = Column(Integer, primary_key = True, nullable=False)
     room_id = Column(ForeignKey('rooms.id'))
@@ -10,6 +12,16 @@ class Bookings(Base):
     date_to = Column(Date, nullable=False)
     price = Column(Integer, nullable=False)
     total_cost = Column(Integer, Computed('(date_from - date_to) * price'))
-    total_days = Column(Integer, Computed('date_from - date_to'))
+    total_days = Column(Integer, Computed('date_from - date_to')) """
 
+class Bookings(Base):
+    __tablename__ = 'bookings'
 
+    id: Mapped[int] = mapped_column(primary_key=True)
+    room_id: Mapped[int] = mapped_column(ForeignKey('rooms.id'))
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    date_from: Mapped[date] = mapped_column(Date)
+    date_to: Mapped[date] = mapped_column(Date)
+    price: Mapped[int]
+    total_cost: Mapped[int] = mapped_column(Computed('(date_from - date_to) * price'))
+    total_days: Mapped[int] = mapped_column(Computed('date_from - date_to'))
--- app/hotels/models.py
+++ app/hotels/models.py
@@ -1,17 +1,29 @@
 from sqlalchemy import JSON, Column, ForeignKey, Integer, String
 from app.database import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from typing import Optional
 
-class Hotels(Base):
+""" class Hotels(Base):
     __tablename__ = 'hotels'
     id= Column(Integer, primary_key = True, nullable=False)
     name = Column(String, nullable = False)
     location= Column(String, nullable = False)
     services= Column(JSON)
     rooms_quantity= Column(Integer, nullable = False)
-    image_id= Column(Integer)
+    image_id= Column(Integer) """
+
+class Hotels(Base):
+    __tablename__ = 'hotels'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str]
+    location: Mapped[str]
+    services: Mapped[list[str]] = mapped_column(JSON)
+    room_quantity: Mapped[int]
+    image_id: Mapped[int]
 
 
-class Rooms(Base):
+""" class Rooms(Base):
     __tablename__ = 'rooms'
 
     id = Column(Integer, primary_key = True, nullable=False)
@@ -21,4 +33,16 @@ class Rooms(Base):
     price = Column(Integer, nullable=False)
     services = Column(JSON, nullable=True)
     quantity = Column(Integer, nullable=False)
-    image_id = Column(Integer)
+    image_id = Column(Integer) """
+
+class Rooms(Base):
+    __tablename__ = 'rooms'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    hotel_id: Mapped[int] = mapped_column(ForeignKey('hotels.id'))
+    name: Mapped[str]
+    description: Mapped[Optional[str]]
+    price: Mapped[int]
+    services: Mapped[list[str]] = mapped_column(JSON)
+    quantity: Mapped[int]
+    image_id: Mapped[int]
