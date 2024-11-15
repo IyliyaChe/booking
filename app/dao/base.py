82dc447c7489159d92fc5d5b7bd199ffbc679dcb
Iyliya Cherepkova <iyliya.cherepkova@gmail.com>
2024-11-15 10:12:14 -0500

    Схема пайдентик для букинга и две функции в дао для поиска пользователя и поиска по id

--- app/dao/base.py
+++ app/dao/base.py
@@ -7,9 +7,23 @@ class BaseDAO:
     model = None
 
     @classmethod
+    async def find_by_id(cls, model_id: int):
+        async with async_session_maker() as session:
+            query = select(cls.model).filter_by(id=model_id)
+            result = await session.execute(query)
+            return result.mappings().one_or_none()
+
+    @classmethod
+    async def find_one_or_none(cls, **filter_by):
+        async with async_session_maker() as session:
+            query = select(cls.model).filter_by(**filter_by)
+            result = await session.execute(query)
+            return result.mappings().one_or_none()
+
+    @classmethod
     async def find_all(cls):
         async with async_session_maker() as session:
             query = select(cls.model)
             result = await session.execute(query)
-            return result.mapping().all()
+            return result.mappings().all()
         
