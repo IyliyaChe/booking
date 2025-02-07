9fd826160986e078d99ecbafec64aee9086ebf29
Iyliya Cherepkova <iyliya.cherepkova@gmail.com>
2025-02-07 10:34:54 -0500

    Добавлена jinja с базовой функциональностью

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ from app.bookings.router import router as router_bookings
 from app.users.router import router as router_users
 from app.hotels.router import router as router_hotels
 from app.hotels.rooms.router import router as router_rooms
+from app.pages.router import router as router_pages
 
 app = FastAPI()
 
@@ -13,6 +14,7 @@ app.include_router(router_users)
 app.include_router(router_hotels)
 app.include_router(router_bookings)
 app.include_router(router_rooms)
+app.include_router(router_pages)
 
 
 """ class SHotel(BaseModel):
