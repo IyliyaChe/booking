2131b3b257cc53b2393c1b278dd7e862a0923e9f
Iyliya Cherepkova <iyliya.cherepkova@gmail.com>
2025-02-10 10:27:37 -0500

    Оформление шаблоны и реализация загрузки файлов

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, Query, Depends
+from fastapi.staticfiles import StaticFiles
+
 from typing import Annotated
 from datetime import date
 from pydantic import BaseModel
@@ -7,14 +9,18 @@ from app.users.router import router as router_users
 from app.hotels.router import router as router_hotels
 from app.hotels.rooms.router import router as router_rooms
 from app.pages.router import router as router_pages
+from app.images.router import router as router_images
 
 app = FastAPI()
+
+app.mount('/static', StaticFiles(directory='app/static'), 'static')
 
 app.include_router(router_users)
 app.include_router(router_hotels)
 app.include_router(router_bookings)
 app.include_router(router_rooms)
 app.include_router(router_pages)
+app.include_router(router_images)
 
 
 """ class SHotel(BaseModel):
